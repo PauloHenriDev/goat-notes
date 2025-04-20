b63cd4c8d75236cb482c4b6712470e9423732dde
Paulo Henrique <paulohenriquerattes@gmail.com>
2025-04-20 17:51:24 -0300

    finaliza o Link com a logo e o nome do site

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,13 +1,28 @@
 import Image from "next/image";
 import Link from "next/link";
 
+import { shadow } from "@/lib/utils";
+
 function Header() {
   return (
-    <div>
-        <Link href="/">
-            <Image src="/goatius.png" height={60} width={60} alt="Logo" className="rounded-full" priority/>
-        </Link>
-    </div>
+    <header className="relative flex h-24 w-full items-center justify-between bg-popover px-3 sm:px-8"
+    style={{
+      boxShadow: shadow,
+    }}
+    >
+      <Link href="/" className="flex items-end gap-2">
+          <Image src="/goatius.png" 
+          height={60} 
+          width={60} 
+          alt="Logo" 
+          className="rounded-full" 
+          priority/>
+
+          <h1 className="flex flex-col pb-1 text-2x1 font-semibold leading-6">
+            GOAT <span>Notes</span>
+          </h1>
+      </Link>
+    </header>
   )
 }
 
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,7 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+const glowColor = "rgba(59, 130, 246, 0.3)"
+
+export const shadow = `0 0 5px ${glowColor}, 0 0 10px ${glowColor}, 0 0 15px ${glowColor}, 0 0 20px ${glowColor}`
